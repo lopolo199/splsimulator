d4b81a083ba90e52064ca756be4272977afa1a95
Franco Moretti <fraanki199@gmail.com>
2025-01-24 13:16:23 -0300

    2.0.8 LFS 8

--- vercel.js
+++ vercel.js
@@ -25,6 +25,33 @@
           "value": "application/octet-stream"
         }
       ]
+    },
+    {
+      "source": "/Build/(.*)",
+      "headers": [
+        {
+          "key": "Content-Type",
+          "value": "application/wasm"
+        }
+      ]
+    },
+    {
+      "source": "/Build/*.data",
+      "headers": [
+        {
+          "key": "Content-Type",
+          "value": "application/octet-stream"
+        }
+      ]
+    },
+    {
+      "source": "/Build/*.framework.js",
+      "headers": [
+        {
+          "key": "Content-Type",
+          "value": "application/javascript"
+        }
+      ]
     }
   ]
 }
